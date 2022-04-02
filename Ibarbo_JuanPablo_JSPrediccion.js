d1f3461e0e7d8838a4911fc51877eea41e493db0
Juan Pablo Ibarbo <juanibarbo456@gmail.com>
2022-04-02 14:25:11 -0500

    version1

---
+++ Ibarbo_JuanPablo_JSPrediccion.js
@@ -0,0 +1,20 @@
+function myBirthYearFunc(){
+        console.log("Nací en" + 1980); // Nací en1980
+    }
+    
+function myBirthYearFunc1(){
+     var b = 1980
+    console.log("Nací en"+ b); // Nací en1980
+}
+
+function add(num1, num2){
+     num1=10
+     num2=20
+    console.log("¡sumando números!"); // ¡sumando números!
+    
+    console.log("num1 es: "+ num1);   // num1 es: 10
+
+    console.log("num2 es: "+ num2); // num2 es: 20
+    var sum = num1 + num2;
+    console.log(sum);      // 30
+}
